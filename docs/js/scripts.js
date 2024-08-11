b1365d015703f0717cd8512cb0eca61605101972
Mauricio Alfaro <mauricio-alfaro@hotmail.com>
2024-08-11 14:12:10 +0200

    Gallery Fix

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -53,7 +53,11 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: '#galeria a.portfolio-box',
+        loadingCaption: 'Cargando...',
+        closeBtnCaption: 'Cerrar',
+        nextBtnCaption: 'Siguiente',
+        prevBtnCaption: 'Anterior',
     });
 
 });
